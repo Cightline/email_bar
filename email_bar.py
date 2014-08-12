0fecb86bcb5fee042ad0d3c17bb3b94afd37b067
Afterburn <mujahideen.solutions@gmail.com>
2014-08-11 21:49:30 -0600

    few options

--- email_bar.py
+++ email_bar.py
@@ -1,40 +1,62 @@
+#!/bin/python
+
 import os
 import configparser
+import argparse
+
 from mailbox import MaildirMessage
 
 config    = configparser.ConfigParser()
-mailboxes = {}
+config_mailboxes = {}
 
 config.read(os.path.expanduser('~/.config/email_bar.cfg'))
 
-for mailbox in config.sections():
-    mailboxes[mailbox] = 0
+parser = argparse.ArgumentParser()
+parser.add_argument('--only',     help='only check specified mailbox', action='store')
+parser.add_argument('--no-title', help='do not display the title', action='store_true')
+
+args = parser.parse_args()
+
+if args.only:
+    config_mailboxes[args.only] = 0
+
+# Else read through the config and check all of the mailboxes
+else:
+    for mailbox in config.sections():
+        config_mailboxes[mailbox] = 0
 
 
-
-for mailbox in mailboxes:
-    m = ['cur','new']
+def check_unread(mailboxes):
+    for mailbox in mailboxes:
+        m = ['cur','new']
     
-    base_path = '%s/INBOX' % (config.get(mailbox, 'path'))
+        base_path = '%s/INBOX' % (config.get(mailbox, 'path'))
 
 
-    full_paths = ['%s/cur' % (base_path), '%s/new' % (base_path)]
+        full_paths = ['%s/cur' % (base_path), '%s/new' % (base_path)]
 
     
-    for path in full_paths:
-        for mail in os.listdir(path):
-            try:
-                with open('%s/%s' % (path, mail)) as msg_file:
-                    msg = MaildirMessage(msg_file.read())
-                    if 'S' not in msg.get_flags():
-                        mailboxes[mailbox] += 1
+        for path in full_paths:
+            for mail in os.listdir(path):
+                try:
+                    with open('%s/%s' % (path, mail)) as msg_file:
+                        msg = MaildirMessage(msg_file.read())
+                        if 'S' not in msg.get_flags():
+                            config_mailboxes[mailbox] += 1
 
-            except Exception as e:
-                #print('[error] %s' % (e))
-                pass
+                except Exception as e:
+                    #print('[error] %s' % (e))
+                    pass
 
-for mailbox in mailboxes.keys():
-    print("%s unread: %s " % (mailbox, mailboxes[mailbox]), end="")
+check_unread(config_mailboxes)
+
+for mailbox in config_mailboxes.keys():
+    if args.no_title:
+        print(config_mailboxes[mailbox])
+
+    
+    else:
+        print("%s: %s " % (mailbox, config_mailboxes[mailbox]), end="")
 
 
 
